a627dd05228d1cfe6feb9fc0ebc6e9b18bade142
Luis Gago Casas <luisgago@lagc-peru.com>
2020-05-08 08:37:45 -0500

    added new services and two sites

---
+++ web1/public/index.php
@@ -0,0 +1,15 @@
+<?php
+$enlace = mysqli_connect("mysqlone", "root", "sunway", "hola");
+
+if (!$enlace) {
+    echo "Error: No se pudo conectar a MySQL." . PHP_EOL;
+    echo "errno de depuración: " . mysqli_connect_errno() . PHP_EOL;
+    echo "error de depuración: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
+
+echo "Éxito: Se realizó una conexión apropiada a MySQL! La base de datos mysqlone es genial." . PHP_EOL;
+echo "Información del host: " . mysqli_get_host_info($enlace) . PHP_EOL;
+
+mysqli_close($enlace);
+?>
